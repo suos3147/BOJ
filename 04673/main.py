5a354494d4042b0602f908e72a06006293bf250a
suos3147 <suos3147@gmail.com>
2019-05-03 18:23:13 +0900

    self number

--- 04673/main.py
+++ 04673/main.py
@@ -4,20 +4,15 @@ self_number = set(range(1,10001))
 
 def remove_not_self_number(n):
     remove_number = n
-    for i in range(n):
-        place_value = n%10
-        remove_number += place_value
+    while n != 0:
+        remove_number += n%10
         n = n//10
-    if remove_number < 10001:
-        self_number.discard(remove_number)
-
-def print_self_number():
-    for i in self_number:
-        print(i)
+    self_number.discard(remove_number)
 
 if __name__ == "__main__":
 
     for i in range(1,10001):
         remove_not_self_number(i)
 
-    print_self_number()
+    for i in self_number:
+        print(i)
