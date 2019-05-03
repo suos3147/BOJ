9e8fdc85b40f044b8c77e3468078dde94f044ddd
suos3147 <suos3147@gmail.com>
2019-05-03 18:14:43 +0900

    self number

--- 04673/main.py
+++ 04673/main.py
@@ -1,3 +1,23 @@
-import sys
+# 문제 출처: https://www.acmicpc.net/problem/4673
+
+self_number = set(range(1,10001))
+
+def remove_not_self_number(n):
+    remove_number = n
+    for i in range(n):
+        place_value = n%10
+        remove_number += place_value
+        n = n//10
+    if remove_number < 10001:
+        self_number.discard(remove_number)
+
+def print_self_number():
+    for i in self_number:
+        print(i)
 
 if __name__ == "__main__":
+
+    for i in range(1,10001):
+        remove_not_self_number(i)
+
+    print_self_number()
