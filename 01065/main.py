551f74093749e9d8b4e4fec32b63b02ec1c61cc8
suos3147 <suos3147@gmail.com>
2019-05-04 23:37:36 +0900

    한수

--- 01065/main.py
+++ 01065/main.py
@@ -1,1 +1,11 @@
 # 문제 출처: https://www.acmicpc.net/problem/1065
+
+import sys
+
+if __name__ == "__main__":
+    while 1:
+        user_input = int(sys.stdin.readline())
+        if(user_input > 0 and user_input <= 1001):
+            break
+    
+    
