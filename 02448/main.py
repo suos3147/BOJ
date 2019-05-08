1f1f4a635b460417a9a5e0d18cb190282fa04b85
suos3147 <suos3147@gmail.com>
2019-05-08 17:41:01 +0900

    print star

--- 02448/main.py
+++ 02448/main.py
@@ -1,4 +1,14 @@
+#문제출처: https://www.acmicpc.net/problem/2448
+
 import sys
 
 if __name__ == "__main__":
-    
+    while 1:
+        user_input = int(sys.stdin.readline())
+        for i in range(11):
+            if user_input == 3*2**i:
+                break
+        if user_input == 3*2**i:
+            break
+        
+    print(user_input)
