54abf35304dc1786d678421cad95ed5a0070406e
suos3147 <suos3147@gmail.com>
2019-05-14 16:44:21 +0900

    scale

--- 02920/main.py
+++ 02920/main.py
@@ -2,10 +2,10 @@
 
 if __name__ == "__main__":
     scale_list = input().split()
-   
-    if scale_list == [1, 2, 3, 4, 5, 6, 7, 8]:
+    
+    if scale_list == ['1', '2', '3', '4', '5', '6', '7', '8']:
         print("ascending")
-    elif scale_list == [8, 7, 6, 5, 4, 3, 2, 1]:
+    elif scale_list == ['8', '7', '6', '5', '4', '3', '2', '1']:
         print("descending")
     else:
         print("mixed")
