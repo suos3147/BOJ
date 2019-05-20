4cd67d737b1f24054ea46bac4f82c41f6b49b89a
suos3147 <suos3147@gmail.com>
2019-05-21 01:21:23 +0900

    ASCII code

--- 11654/main.py
+++ 11654/main.py
@@ -1,2 +1,18 @@
 # 문제출처: https://www.acmicpc.net/problem/11654
 
+if __name__ == "__main__":
+
+    while 1:
+        try:
+            user_input = ord(input())
+            if user_input >= 48 and user_input <= 57:
+                break
+            elif user_input >= 65 and user_input <= 90:
+                break
+            elif user_input >= 97 and user_input <=122:
+               break
+
+        except TypeError:
+            continue
+    
+    print(user_input)
