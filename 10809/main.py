239cc95b6ccace27ba74630f79c888fc2d50a0c7
suos3147 <suos3147@gmail.com>
2019-05-23 23:54:26 +0900

    search alphabet

--- 10809/main.py
+++ 10809/main.py
@@ -2,14 +2,19 @@
 import sys
 
 if __name__ == "__main__":
+    print_list = [0]*26
     user_input = list(input())
 
     alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
-    if user_input[0] == alphabet[0]:
-        alphabet[0] = user_input.index(alphabet[0])
-    else:
-        alphabet[0] = -1
-
-    print(alphabet)
+    for i in range(len(alphabet)):
+        for j in range(len(user_input)):
+            if user_input[j] == alphabet[i]:
+                print_list[i] = user_input.index(alphabet[i])
+                break
+            else:
+                print_list[i] = -1
+        
+    for i in range(len(print_list)):
+        sys.stdout.write(str(print_list[i])+" ")
     
