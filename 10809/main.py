0a3bc75694223bb09ec1c0f16b5fbbc6bdfc6ff4
suos3147 <suos3147@gmail.com>
2019-05-23 23:26:52 +0900

    alpahbet

--- 10809/main.py
+++ 10809/main.py
@@ -1,1 +1,14 @@
 # 문제출처: https://www.acmicpc.net/problem/10809
+import sys
+
+if __name__ == "__main__":
+    user_input = list(input())
+
+    alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+    for i in range(len(user_input)):
+        for j in range 26:
+            if user_input[i] == alphabet[j]:
+                alphabet[j] = user_input.index()
+            else:
+                alphabet[j] = -1             
