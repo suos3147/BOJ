0439a561368ee2beadc0a624b2a4abf81561a1ea
suos3147 <suos3147@gmail.com>
2019-06-03 16:30:33 +0900

    average score

--- 10039/main.py
+++ 10039/main.py
@@ -1,1 +1,20 @@
 # 문제출처: https://www.acmicpc.net/problem/10039
+def enter_score():
+    while 1:
+        score = int(input())
+        if score >= 0 and score <= 100:
+            break
+        else:
+            continue
+    return score
+
+if __name__ == "__main__":
+    total_score = 0
+
+    for i in range(5):
+        score = enter_score()
+        if score < 40:
+            score = 40
+        total_score += score
+
+    print(total_score//5)
