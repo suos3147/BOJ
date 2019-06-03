0aa733773fc2475a50e1118ed5035033be5256da
suos3147 <suos3147@gmail.com>
2019-06-03 15:29:34 +0900

    OX quiz

--- 08958/main.py
+++ 08958/main.py
@@ -1,9 +1,17 @@
 # 문제출처: https://www.acmicpc.net/problem/8958
 
 if __name__ == "__main__":
-    num = int(input())
-
-    while num != 0:
-        user_input = input()
-        
-        
+    testcase = []
+    test_count = int(input())
+    
+    for i in range(test_count):
+        score = 0
+        total_score = 0
+        testcase.append(list(input()))
+        for j in range(len(testcase[i])):
+            if testcase[i][j] == 'O':
+                score += 1
+                total_score += score
+            else:
+                score = 0
+        print(total_score)
