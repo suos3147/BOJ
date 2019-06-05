42dc63772f2e30110ccff217504979cf4765069a
suos3147 <suos3147@gmail.com>
2019-06-05 18:22:08 +0900

    stack

--- 10828/main.py
+++ 10828/main.py
@@ -1,1 +1,12 @@
 # 문제출처: https://www.acmicpc.net/problem/10828
+
+def enter_command_count():
+    while 1:
+        command_line = int(input())
+        if command_line >= 0 and command_line <=10000:
+                return command_line
+        else:
+                continue
+
+if __name__ == "__main__":
+    print(enter_command_count())
