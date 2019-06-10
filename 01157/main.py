74b8ce93a79e74424e2085da859918a11235f7b4
suos3147 <suos3147@gmail.com>
2019-06-10 22:56:29 +0900

    word study

--- 01157/main.py
+++ 01157/main.py
@@ -1,1 +1,13 @@
 # 문제출처: https://www.acmicpc.net/problem/1157
+def enter_string():
+    try:
+        str = input().upper()
+    except ValueError:
+        return enter_string
+    if len(str) <= 1000000:
+        return str
+    else:
+        return enter_string
+
+if __name__ == "__main__":
+    enter_string()
