b680d4532e9069631a373a61e88257f25f6d31f5
suos3147 <suos3147@gmail.com>
2019-07-04 22:30:48 +0900

    baekjoon

--- 09012/09012.py
+++ 09012/09012.py
@@ -1,7 +1,14 @@
 # 문제출저: https://www.acmicpc.net/problem/9012
 
+def input_ps(number):
+    while number != 0:
+            ps = input()
+            if len(ps) >= 2 and len(ps) <= 50:
+                number -= 1
+            else:
+                return ps
+
 if __name__ == "__main__":
     NUMBER = int(input())
 
-    while NUMBER != 0:
-        ps = input()
+    input_ps(NUMBER)
